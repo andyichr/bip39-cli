48f98cf2c390e1fa3a35e15055029a5cc5ac0697
Andy Christianson <andy@andyic.org>
2015-03-17 21:52:51 -0400

    added initial working implementation

---
+++ Makefile
@@ -0,0 +1,57 @@
+CC        = gcc
+
+OPTFLAGS  = -Os -g
+
+CFLAGS   += $(OPTFLAGS) \
+            -W \
+            -Wall \
+            -Wextra \
+            -Wimplicit-function-declaration \
+            -Wredundant-decls \
+            -Wstrict-prototypes \
+            -Wundef \
+            -Wshadow \
+            -Wpointer-arith \
+            -Wformat \
+            -Wreturn-type \
+            -Wsign-compare \
+            -Wmultichar \
+            -Wformat-nonliteral \
+            -Winit-self \
+            -Wuninitialized \
+            -Wformat-security \
+            -Werror
+
+# disable sequence point warning because of AES code
+CFLAGS += -Wno-sequence-point
+
+# disable certain optimizations and features when small footprint is required
+ifdef SMALL
+CFLAGS += -DUSE_PRECOMPUTED_IV=0 -DUSE_PRECOMPUTED_CP=0
+endif
+
+OBJS   = bip39.o \
+         sha2.o \
+				 rand.o \
+				 pbkdf2.o \
+				 hmac.o \
+				 bip32.o \
+				 ecdsa.o \
+				 bignum.o \
+				 secp256k1.o \
+				 base58.o \
+				 ripemd160.o
+
+all: bip39-cli
+
+%.o: %.c %.h options.h
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+bip39-cli.o: bip39-cli.c
+	$(CC) $(CFLAGS) -c bip39-cli.c
+
+bip39-cli: $(OBJS) bip39-cli.o
+	$(CC) $(CFLAGS) -o bip39-cli bip39-cli.o $(OBJS)
+
+clean:
+	rm -f *.o tests test-openssl
---
+++ bip39-cli.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "bip39.h"
+#include "rand.h"
+#include "bip32.h"
+
+#define LOGO "    ██████╗ ██╗██████╗ ██████╗  █████╗        ██████╗██╗     ██╗\n"\
+             "    ██╔══██╗██║██╔══██╗╚════██╗██╔══██╗      ██╔════╝██║     ██║\n"\
+             "    ██████╔╝██║██████╔╝ █████╔╝╚██████║█████╗██║     ██║     ██║\n"\
+             "    ██╔══██╗██║██╔═══╝  ╚═══██╗ ╚═══██║╚════╝██║     ██║     ██║\n"\
+             "    ██████╔╝██║██║     ██████╔╝ █████╔╝      ╚██████╗███████╗██║\n"\
+             "    ╚═════╝ ╚═╝╚═╝     ╚═════╝  ╚════╝        ╚═════╝╚══════╝╚═╝\n"
+
+int main(void) {
+
+  int i;
+  uint8_t seed[512 / 8];
+  const char *mnemonic;
+  char passphrase[2056];
+  char input_mnemonic[2056];
+  char xprv_string[1024];
+  char xpub_string[1024];
+
+  // output logo
+  printf("\n\n%s\n", LOGO);
+
+  // read mnemonic
+  printf("enter mnemonic (leave empty to generate): ");
+  fgets(input_mnemonic, 2056, stdin);
+
+  i = strlen(input_mnemonic) - 1;
+
+  if (input_mnemonic[i] == '\n') {
+    input_mnemonic[i] = '\0';
+  }
+
+  if (strlen(input_mnemonic) > 0) {
+    mnemonic = input_mnemonic;
+  } else {
+    // generate mnemonic
+    init_rand();
+    mnemonic = mnemonic_generate(128);
+    printf("generated mnemonic: %s\n", mnemonic);
+    finalize_rand();
+  }
+
+  if (!mnemonic_check(mnemonic)) {
+    fputs("mnemonic is invalid\n", stderr);
+    return 1;
+  }
+
+  // read passphrase
+  printf("enter passphrase (optional): ");
+  fgets(passphrase, 2056, stdin);
+
+  i = strlen(passphrase) - 1;
+
+  if (passphrase[i] == '\n') {
+    passphrase[i] = '\0';
+  }
+
+  // generate seed
+  mnemonic_to_seed(mnemonic, passphrase, seed, 0);
+
+  // generate HD node
+  HDNode node;
+  hdnode_from_seed(seed, 512 / 8, &node);
+
+  // serialize and output master private key
+  hdnode_serialize_private(&node, xprv_string, 1024);
+  printf("master private key: %s\n", xprv_string);
+
+  // serialize and output master public key
+  hdnode_serialize_public(&node, xpub_string, 1024);
+  printf("master public key: %s\n", xpub_string);
+
+  return 0;
+
+}
